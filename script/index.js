15633a174c10c310741eed0a7a4820b388d7cfc3
Ruslan Minakov <minakov.ruslan@gmail.com>
2022-05-09 23:05:01 +0300

    sprint 4 final

---
+++ script/index.js
@@ -0,0 +1,29 @@
+let popup = document.querySelector('.popup');
+let editButton = document.querySelector('.profile__edit-button');
+let closeButton = document.querySelector('.popup__close-button');
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('#username');
+let jobInput = document.querySelector('#about');
+let profileName = document.querySelector('.profile__title');
+let profileJob = document.querySelector('.profile__subtitle');
+
+function openPopup() {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+  popup.classList.add('popup_opened');
+}
+
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  closePopup();
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
